eb2ae4978c2f40538cfe665f5404c331fd3dd71b
Eric <erictoribio2010@gmail.com>
2022-05-25 12:30:50 -0400

    completed json

--- src/main/java/ip/management/erictoribio/models/IPModel.java
+++ src/main/java/ip/management/erictoribio/models/IPModel.java
@@ -1,8 +1,6 @@
 package ip.management.erictoribio.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
